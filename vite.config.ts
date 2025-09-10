dcacc397d0e899223ab6a327a6545c74858ae5eb
Sowjanya Pilla <105363948+sowjanyapilla@users.noreply.github.com>
2025-09-10 17:56:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/", // or "./" if not hosted at root (e.g. GitHub Pages)
+  build: {
+    outDir: "dist",       // folder where build files go
+    sourcemap: false,     // set to true only if you need debugging in prod
+    minify: "esbuild",    // fast minification (default)
+  },
+  // optimizeDeps: {
+  //   exclude: ["lucide-react"],
+  // },
 })
